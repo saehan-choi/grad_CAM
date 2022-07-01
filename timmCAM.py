a12f5b8cce0ece57ee52eb9e2ab5f7c6053dbdca
SeHan Choi <shchoi@DI-SOLUTION.CO.KR>
2022-07-01 16:23:18 +0900

    test

--- timmCAM.py
+++ timmCAM.py
@@ -1,18 +1,31 @@
+from pyexpat import model
 import torch.nn as nn
 import torch
 import timm
 
+class CFG:
+    clsarr = ['nomask', 'mask', 'wrong', 'blind']
+    classes = 4
 
 class efficientnet_b0:
     def __init__(self):
         super(efficientnet_b0, self).__init__()
         
-        model = timm.create_model('efficientnet_b0')
-        model.global_pool = nn.Identity()
-        model.classifier = nn.Identity()
-
-        randn = torch.randn(1,3,256,256)
-        print(model(randn).size())
+        self.model = timm.create_model('efficientnet_b0')
+        self.global_pool = self.model.global_pool
+        self.classifier = nn.Linear(1280, CFG.classes)
+        self.model.global_pool = nn.Identity()
+        self.model.classifier = nn.Identity()
+        
+        self.gradient = None
+        
+    def forward(self, x):
+        x = self.model(x)
+        x = self.global_pool(x)
+        x = self.classifier(x)
+        
+        # input:256 -> ([1, 1280, 8, 8])
+        
         
         
         
